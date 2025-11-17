de29fb024c40b8bbf3f61d2260a36b76d94af34c
Alessio <alexeimv91@gmail.com>
2025-11-16 21:50:16 -0500

    fix(paginate): Cambiando la logica para que desde el paginate se filtren o no los eliminados.

--- app/common/utils/paginate.py
+++ app/common/utils/paginate.py
@@ -2,16 +2,23 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, func
 
 
-async def paginate(session: AsyncSession, model, page: int, page_size: int):
+async def paginate(session: AsyncSession, model, page: int, page_size: int, is_deleted=False):
     offset = (page - 1) * page_size
 
-    total = await session.scalar(
-        select(func.count()).select_from(model)
-    )
-
-    query = await session.execute(
-        select(model).offset(offset).limit(page_size)
-    )
+    if hasattr(model, "is_deleted"):
+        total = await session.scalar(
+            select(func.count()).select_from(model).where(model.is_deleted == is_deleted)
+        )
+        query = await session.execute(
+            select(model).where(model.is_deleted == is_deleted).offset(offset).limit(page_size)
+        )
+    else:
+        total = await session.scalar(
+            select(func.count()).select_from(model)
+        )
+        query = await session.execute(
+            select(model).offset(offset).limit(page_size)
+        )
 
     return {
         "items": query.scalars().all(),
